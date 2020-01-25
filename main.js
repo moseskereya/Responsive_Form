7f8866c6015d03dd8bb23b273547c5fa34be3992
Moses <lazaromoses77@gmail.com>
2020-01-25 05:09:07 +0300

    adding more js animations

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 window.sr = ScrollReveal();
 
 sr.reveal('.animate-left' ,{
-    origin:'left',
+    origin:'bottom',
+    duration:1000,
+    distance:'25rem',
+    delay:300
+})
+
+sr.reveal('.animate' ,{
+    origin:'top',
     duration:1000,
     distance:'25rem',
     delay:300
